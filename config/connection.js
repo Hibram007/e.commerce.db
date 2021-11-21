2d8eec54cfdb6bbf2a3a75b090faad22f7138133
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-20 19:54:49 -0700

    all routes and models finalized, current error with Sequelize- instance not passed

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', '1998Hlsg8sql', {
+const sequelize = new Sequelize('ecommerce_db', 'root', '1998Hlsg8sql', 
+{
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+    })
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection')
 
+// cod eto initialize the app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+// code to start the server- db connection
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
